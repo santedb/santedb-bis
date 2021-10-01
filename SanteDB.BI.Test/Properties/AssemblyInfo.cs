8b99e7cbe767908b5c560d2210e7cd18fec4e75d
Justin Fyfe <justin@fyfesoftware.ca>
2021-09-30 20:11:20 -0400

    Update copyright notice - see NOTICE.md

--- SanteDB.BI.Test/Properties/AssemblyInfo.cs
+++ SanteDB.BI.Test/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SanteDB.BI.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright (C) 2021 SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
