e3e710fddd0f2ae5cfd04f588d3ea2bc4252593d
Justin Fyfe <justin@fyfesoftware.ca>
2021-10-19 17:18:12 -0400

    Updated matching service routines to properly sticky objects - Updated caching routines to remove extended ITargetedAssociation relations

--- SanteDB.BI/Services/IBiReportFormatProvider.cs
+++ SanteDB.BI/Services/IBiReportFormatProvider.cs
@@ -2,22 +2,23 @@
  * Copyright (C) 2021 - 2021, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
  * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
  * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you 
- * may not use this file except in compliance with the License. You may 
- * obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations under 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * User: fyfej
  * Date: 2021-8-5
  */
+
 using SanteDB.BI.Model;
 using System;
 using System.Collections.Generic;
@@ -30,7 +31,6 @@ namespace SanteDB.BI.Services
     /// </summary>
     public interface IBiReportFormatProvider
     {
-
         /// <summary>
         /// Render the specified report accoring to the format
         /// </summary>
@@ -39,7 +39,5 @@ namespace SanteDB.BI.Services
         /// <param name="viewName">The name of the view to berendered</param>
         /// <returns>The rendered output stream</returns>
         Stream Render(BiReportDefinition reportDefinition, String viewName, IDictionary<String, Object> parameters);
-
-
     }
 }
