94635fdfbd1a16f820c2633463995ae9cf312231
Justin Fyfe <justin@fyfesoftware.ca>
2024-03-05 14:29:30 -0500

    Ran code cleanup - and updated the license headers

--- SanteDB.BI/Components/ReportViewUtil.cs
+++ SanteDB.BI/Components/ReportViewUtil.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (C) 2021 - 2023, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
+ * Copyright (C) 2021 - 2024, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
  * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
  * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
  * 
@@ -16,7 +16,7 @@
  * the License.
  * 
  * User: fyfej
- * Date: 2023-5-19
+ * Date: 2023-6-21
  */
 using DynamicExpresso;
 using SanteDB.BI.Exceptions;
@@ -27,7 +27,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml;
 using System.Xml.Linq;
